0e256e975f36d9bd1d91e48553d98f9fa3b54be7
balapkm <balakumaran.raji@gmail.com>
2019-06-19 10:55:59 +0530

    Add existing file

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Circle CI</title>
-</head>
-<body>
-<?php echo "string"; ?>
-</body>
-</html>
+<?php
+
+$servername = "localhost";
+$username = "myuser";
+$password = "password";
+try {
+    $conn = new PDO("mysql:host=$servername", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully\n";
+}
+catch(PDOException $e)
+{
+    echo "Connection failed: " . $e->getMessage() . "\n";
+    die(1);
+}
+
+?>
