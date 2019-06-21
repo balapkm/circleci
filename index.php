6328ab9b8f6f81def04b40f0a8ad1c4ccd8cb54e
balakumaran <balakumaran.raji@gmail.com>
2019-06-21 10:33:19 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ try {
     $conn = new PDO("mysql:host=$servername", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully circleci run successfully 111\n";
+    echo "Connected successfully circleci run successfullddy 111\n";
 }
 catch(PDOException $e)
 {
