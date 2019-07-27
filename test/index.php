4bda412a76b52fc993787a6abb49ad063cedb0b7
balakumaran <balakumaran.raji@gmail.com>
2019-07-27 19:56:36 +0530

    dddddddddddddd

---
+++ test/index.php
@@ -0,0 +1,19 @@
+<?php
+echo "3 st change Madam HElo successfully sybase_data_seek(result_identifier, row_number)  ";
+// exit;
+$servername = "localhost";
+$username = "root";
+$password = "Bala@123";
+try {
+    $conn = new PDO("mysql:host=$servername", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully circleci run successfullddy  vvvv ff 111\n";
+}
+catch(PDOException $e)
+{
+    echo "Connection failed: " . $e->getMessage() . "\n";
+    die(1);
+}
+
+?>
